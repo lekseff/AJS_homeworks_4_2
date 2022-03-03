40511a0d1fd7fee31a3f65cf89176f39bc7fc468
Aleksey Zayachnikov <lekseff@gmail.com>
2022-03-03 22:31:27 +0300

    add const HERO_TYPES class Character

--- src/js/module/Character.js
+++ src/js/module/Character.js
@@ -1,5 +1,13 @@
+const HERO_TYPES = {
+  BOWMAN: 'Bowman',
+  SWORDSMAN: 'Swordsman',
+  MAGICIAN: 'Magician',
+  DAEMON: 'Daemon',
+  UNDEAD: 'Undead',
+  ZOMBIE: 'Zombie',
+};
+
 export default class Character {
-  // static types = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
   constructor(name, type) {
     if (name.length < 2 || name.length > 10) {
       throw new Error('Неверное имя героя');
@@ -7,7 +15,7 @@ export default class Character {
       this.name = name;
     }
 
-    if (!(['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'].includes(type))) {
+    if (!Object.values(HERO_TYPES).includes(type)) {
       throw new Error('Неверный тип героя');
     } else {
       this.type = type;
